6a58aa828ed152f970e01a04b853e01e6e71dcc2
Jordy500 <156438783+Jordy500@users.noreply.github.com>
2024-12-02 17:01:18 +0100

    Ajout de nouvelles fichiers et modification de main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
-# Lecture du fichier CSV
+
 df = pd.read_csv('database.csv')
 
+ndf = df.drop(['ID','Year_Birth','Z_Revenue', 'Z_CostContact', 'Complain','Education', 'Marital_Status', 'Dt_Customer'], axis=1, inplace=True)
+
+print(df.info())
+print(df.describe())
 print(df.head())
+
